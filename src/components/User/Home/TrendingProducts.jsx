e337dbbcef7ee0e16609fcf8b571a605818fd454
philtechphilips <69679124+philtechphilips@users.noreply.github.com>
2023-08-09 03:01:27 +0100

    User home trending-products refactored

--- src/components/User/Home/TrendingProducts.jsx
+++ src/components/User/Home/TrendingProducts.jsx
@@ -4,51 +4,51 @@ const TrendingProducts = () => {
     const trendingProducts = [
         {
             image: "https://ng.jumia.is/cms/0-1-homepage/0-0-freelinks-gray/300x240/phones-tablets_300x240.png",
-            category: "Phones & Tablet"
+            name: "Phones & Tablet"
         },
         {
             image: "https://ng.jumia.is/cms/0-1-homepage/0-0-freelinks-gray/300x240/groceries_300x240v2.png",
-            category: "Groceries"
+            name: "Groceries"
         },
         {
             image: "https://ng.jumia.is/cms/0-1-homepage/0-0-freelinks-gray/300x240/generic_300x240.jpg",
-            category: "Computers"
+            name: "Computers"
         },
         {
             image: "https://ng.jumia.is/cms/0-6-anniversary/2023/Brand-day/30-Nivea/generic_300x240.jpg",
-            category: "Health & Beauty"
+            name: "Health & Beauty"
         },
         {
             image: "https://ng.jumia.is/cms/0-1-homepage/0-0-freelinks-gray/300x240/televisions_300x240.png",
-            category: "Televisions"
+            name: "Televisions"
         },
         {
             image: "https://ng.jumia.is/cms/0-1-homepage/0-0-freelinks-gray/300x240/fash_300x240.jpg",
-            category: "Fashions"
+            name: "Fashions"
         },
         {
             image: "https://ng.jumia.is/cms/0-1-homepage/0-0-freelinks-gray/300x240/refrigerators_300x240.png",
-            category: "Refrigerators"
+            name: "Refrigerators"
         },
         {
             image: "https://ng.jumia.is/cms/0-1-homepage/0-0-freelinks-gray/300x240/earphones_300x240.png",
-            category: "Mobile Accessories"
+            name: "Mobile Accessories"
         },
         {
             image: "https://ng.jumia.is/cms/0-1-homepage/0-0-freelinks-gray/300x240/generator.gif",
-            category: "Generators"
+            name: "Generators"
         },
         {
             image: "https://ng.jumia.is/cms/0-1-homepage/0-0-freelinks-gray/300x240/fashion_300x240.png",
-            category: "Men's Sneakers"
+            name: "Men's Sneakers"
         },
         {
             image: "https://ng.jumia.is/cms/0-1-homepage/0-0-freelinks-gray/300x240/watches_300x240.png",
-            category: "Watches"
+            name: "Watches"
         },
         {
             image: "https://ng.jumia.is/cms/0-1-initiatives/clearance-sale/2023/Clearance.gif",
-            category: "Clearance Sales"
+            name: "Clearance Sales"
         },
 
     ]
@@ -95,8 +95,14 @@ const TrendingProducts = () => {
                     {visibleProducts.map((product, index) => (
                         <div className='bg-gray-100 rounded-xl hover:drop-shadow-lg w-48 md:w-72 mb-4' key={index}>
                             <img className='rounded' src={product.image} alt={`Trending Product ${index}`} />
-                            <div className='p-400 text-sm text-center mt-2 bg-white'>
-                                {product.category}
+                            <div className='mt-2 flex items-center justify-between bg-white p-3'>
+                                <div>
+                                    <p className='p-700 text-sm text-gray-700'>{product.name}</p>
+                                    <p className='p-700 text-lg text-gray-800'>&#8358; 500</p>
+                                </div>
+                                <div className='p-1 h-9 w-9 flex items-center justify-center rounded-full bg-gray-900'>
+                                    <i className="ri-shopping-cart-line text-lg text-white"></i>
+                                </div>
                             </div>
                         </div>
                     ))}
