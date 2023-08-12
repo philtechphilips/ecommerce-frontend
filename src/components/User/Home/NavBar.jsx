efc1d9c53783e3da463c6c086dc75eb0765142ea
philtechphilips <69679124+philtechphilips@users.noreply.github.com>
2023-08-12 19:45:55 +0100

    User navbar refactored

--- src/components/User/Home/NavBar.jsx
+++ src/components/User/Home/NavBar.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const NavBar = () => {
+    const [isFixed, setIsFixed] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsFixed(window.scrollY > 100);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return (
-        <div className='h-12 hidden md:flex justify-between items-center px-10 py-12'>
+        <div className={`h-8  bg-white flex justify-between items-center px-10 py-8 drop-shadow-sm mb-5 ${isFixed ? 'fixed w-full top-0 z-[1000]' : ''}`}>
             <Link to="/" className='logo text-4xl text-[#0C513F]'>NOSTRA</Link>
 
-            <ul className="flex justify-between gap-10 items-center">
+            <ul className="hidden md:flex justify-between gap-10 items-center">
                 <li className="p-600 text-gray-800"><Link to="/shop">Shop</Link></li>
                 <li className="p-600 text-gray-800"><Link to="/#">Most Wanted</Link></li>
                 <li className="p-600 text-gray-800"><Link to="/#">New Arrival</Link></li>
                 <li className="p-600 text-gray-800"><Link to="/#">Brands</Link></li>
             </ul>
 
-            <div className="flex items-center gap-10">
+            <div className="hidden md:flex items-center gap-10">
                 <form className="flex items-center bg-gray-100 border border-gray-300 rounded-lg p-2">
                     <i className="ri-search-line text-lg text-gray-400 mr-1 cursor-pointer"></i>
                     <input
@@ -27,6 +39,10 @@ const NavBar = () => {
                     <i className="ri-shopping-cart-2-line text-2xl"></i>
                     <i className="ri-user-3-line text-2xl"></i>
                 </div>
+            </div>
+            <div className="flex items-center gap-4 md:hidden">
+                <i className="ri-shopping-cart-2-line text-2xl"></i>
+                <i className="ri-user-3-line text-2xl"></i>
             </div>
         </div>
     )
